4ce40b299f0bee9231e203992c6d74aa6e990c00
imgruntw <imgruntw@gmail.com>
2018-07-15 00:03:10 +0200

    line tracking

--- robotcar/robotcar.ino
+++ robotcar/robotcar.ino
@@ -1,5 +1,12 @@
+#include <IRremote.h>
+#include <Servo.h>
 
-// the direction of the car's movement
+// line tracking IO
+#define LT_R digitalRead(10)
+#define LT_M digitalRead(4)
+#define LT_L digitalRead(2)
+
+// define L298n module IO Pin
 // ENA   ENB   IN1   IN2   IN3   IN4   Description
 // HIGH  HIGH  HIGH  LOW   LOW   HIGH  Car is runing forward
 // HIGH  HIGH  LOW   HIGH  HIGH  LOW   Car is runing back
@@ -8,276 +15,44 @@
 // HIGH  HIGH  LOW   LOW   LOW   LOW   Car is stoped
 // HIGH  HIGH  HIGH  HIGH  HIGH  HIGH  Car is stoped
 // LOW   LOW   N/A   N/A   N/A   N/A   Car is stoped
-
-#include <IRremote.h>
-#include <Servo.h>
-
-// define L298n module IO Pin
-#define ENA 5
-#define ENB 6
-#define IN1 7
-#define IN2 8
-#define IN3 9
-#define IN4 11
+const byte ENA = 5;
+const byte ENB = 6;
+const byte IN1 = 7;
+const byte IN2 = 8;
+const byte IN3 = 9;
+const byte IN4 = 11;
 
 // infrared signal codes
-#define RECV_PIN 12
-#define IR_UP 16736925
-#define IR_DOWN 16754775
-#define IR_LEFT 16720605
-#define IR_RIGHT 16761405
-#define IR_OK 16712445
-#define IR_ONE 16738455
-#define IR_TWO 16750695
+const byte RECV_PIN = 12;
+const long IR_UP = 16736925;
+const long IR_DOWN = 16754775;
+const long IR_LEFT = 16720605;
+const long IR_RIGHT = 16761405;
+const long IR_OK = 16712445;
+const long IR_ONE = 16738455;
+const long IR_TWO = 16750695;
 
+// infrared signal
 IRrecv irrecv(RECV_PIN);
 decode_results irResults;
-unsigned long irTimestampMs;
 
-Servo obstacleServo;
-int echo = A4;
-int trig = A5;
+// servo and obstacle detection
+const int ECHO = A4;
+const int TRIG = A5;
+const int MIN_OBSTACLE_DISTANCE = 20;
 int rightDistance = 0;
 int leftDistance = 0;
 int middleDistance = 0;
 bool servoActive = false;
-int maxDistance = 30;
+Servo obstacleServo;
 
-void accelerate() {
-  for (int i = 50; i <= 255; i++) {
-    analogWrite(ENA, i);
-    analogWrite(ENB, i);
-    delay(10);
-  }
-}
-
-void decelerate() {
-  for (int i = 255; i >= 0; i--) {
-    analogWrite(ENA, i);
-    analogWrite(ENB, i);
-    delay(10);
-  }
-}
-
-void throttle() {
-  digitalWrite(ENA, HIGH);
-  digitalWrite(ENB, HIGH);
-}
-
-void hold() {
-  digitalWrite(ENA, LOW);
-  digitalWrite(ENB, LOW);
-  digitalWrite(IN1, LOW);
-  digitalWrite(IN2, LOW);
-  digitalWrite(IN3, LOW);
-  digitalWrite(IN4, LOW);
-
-  Serial.println("stop");
-}
-
-void forward(bool analog) {
-  digitalWrite(IN1, HIGH);
-  digitalWrite(IN2, LOW);
-  digitalWrite(IN3, LOW);
-  digitalWrite(IN4, HIGH);
-
-  if (analog) {
-    accelerate();
-  }
-  else {
-    throttle();
-  }
-
-  Serial.println("forward");
-
-  avoidObstacle();
-}
-
-void back(bool analog) {
-  digitalWrite(IN1, LOW);
-  digitalWrite(IN2, HIGH);
-  digitalWrite(IN3, HIGH);
-  digitalWrite(IN4, LOW);
-
-  if (analog) {
-    accelerate();
-  }
-  else {
-    throttle();
-  }
-
-  Serial.println("back");
-}
-
-void left() {
-  digitalWrite(IN1, LOW);
-  digitalWrite(IN2, HIGH);
-  digitalWrite(IN3, LOW);
-  digitalWrite(IN4, HIGH);
-
-  throttle();
-
-  Serial.println("left");
-}
-
-void right() {
-  digitalWrite(IN1, HIGH);
-  digitalWrite(IN2, LOW);
-  digitalWrite(IN3, HIGH);
-  digitalWrite(IN4, LOW);
-
-  throttle();
-
-  Serial.println("right");
-}
-
-void signaling() {
-  digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
-
-  Serial.println("light");
-}
-
-int measureDistance() {
-  digitalWrite(trig, LOW);
-  delayMicroseconds(2);
-  digitalWrite(trig, HIGH);
-  delayMicroseconds(20);
-  digitalWrite(trig, LOW);
-
-  float distance = pulseIn(echo, HIGH);
-  distance = distance / 58;
-
-  return (int) distance;
-}
-
-void obstacleAutoDrive() {
-  if (servoActive) {
-    obstacleServo.attach(3);
-    obstacleServo.write(90);
-    middleDistance = measureDistance();
-
-    if (middleDistance <= maxDistance) {
-      hold();
-      delay(500);
-      obstacleServo.write(10);
-      delay(1000);
-      rightDistance = measureDistance();
-
-      delay(500);
-      obstacleServo.write(90);
-      delay(1000);
-      obstacleServo.write(180);
-      delay(1000);
-      leftDistance = measureDistance();
-
-      delay(500);
-      obstacleServo.write(90);
-      delay(1000);
-
-      if (rightDistance > leftDistance) {
-        right();
-        delay(200);
-      }
-      else if (rightDistance < leftDistance) {
-        left();
-        delay(200);
-      }
-      else if ((rightDistance <= maxDistance) || (leftDistance <= maxDistance)) {
-        back(false);
-        delay(180);
-      }
-      else {
-        forward(false);
-      }
-    }
-    else {
-      forward(false);
-    }
-  }
-}
-
-void avoidObstacle() {
-  int distance = measureDistance();
-
-  if (distance <= 10) {
-    hold();
-  }
-}
-
-void controlBluetooth() {
-  // bluetooth serial port
-  char bluetooth = Serial.read();
-
-  switch (bluetooth) {
-    case 'f':
-      forward(true);
-      break;
-    case 'b':
-      back(true);
-      break;
-    case 'l':
-      left();
-      break;
-    case 'r':
-      right();
-      break;
-    case 'h':
-      hold();
-      break;
-    case 's':
-      signaling();
-      break;
-    case 'o':
-      servoActive = !servoActive;
-      break;
-    default:
-      break;
-  }
-}
-
-void controlIr() {
-  if (irrecv.decode(&irResults)) {
-    irTimestampMs = millis();
-
-    unsigned long irValue = irResults.value;
-    Serial.println(irValue);
-    irrecv.resume();
-
-    switch (irValue) {
-      case IR_UP:
-        forward(true);
-        break;
-      case IR_DOWN:
-        back(true);
-        break;
-      case IR_LEFT:
-        left();
-        break;
-      case IR_RIGHT:
-        right();
-        break;
-      case IR_OK:
-        hold();
-        break;
-      case IR_ONE:
-        signaling();
-        break;
-      case IR_TWO:
-        servoActive = !servoActive;
-        break;
-      default:
-        break;
-    }
-  }
-  else {
-    unsigned long pastMs = millis() - irTimestampMs;
-
-    if (pastMs > 5000) {
-      hold();
-      irTimestampMs = millis();
-    }
-  }
-}
+// control
+const char FRW = 'f';
+const char BCK = 'b';
+const char LFT = 'l';
+const char RGH = 'r';
+const char HLD = 'h';
+const char LED = 's';
 
 void setup() {
   // open serial and set the baudrate
@@ -291,18 +66,171 @@ void setup() {
   pinMode(IN4, OUTPUT);
   pinMode(ENA, OUTPUT);
   pinMode(ENB, OUTPUT);
-
-  // servo init
-  pinMode(echo, INPUT);
-  pinMode(trig, OUTPUT);
+  pinMode(ECHO, INPUT);
+  pinMode(TRIG, OUTPUT);
+  pinMode(LT_R, INPUT);
+  pinMode(LT_M, INPUT);
+  pinMode(LT_L, INPUT);
 
   // start receiving infrared signal
   irrecv.enableIRIn();
+
+  // servo activate
+  if (servoActive) {
+    obstacleServo.attach(3);
+    obstacleServo.write(90);
+  }
 }
 
 void loop() {
-  controlBluetooth();
-  controlIr();
-  obstacleAutoDrive();
+  char bluetoothCommand = readBluetooth();
+  char irCommand = readIr();
+  bool forwardCommand = FRW == bluetoothCommand || FRW == irCommand;
+  bool backCommand = BCK == bluetoothCommand || BCK == irCommand;
+  bool leftCommand = LFT == bluetoothCommand || LFT == irCommand;
+  bool rightCommand = RGH == bluetoothCommand || RGH == irCommand;
+  bool holdCommand = HLD == bluetoothCommand || HLD == irCommand;
+  bool ledCommand = LED == bluetoothCommand || LED == irCommand;
+  bool obstacle = measureDistance() <= MIN_OBSTACLE_DISTANCE;
+
+  if (forwardCommand && !obstacle) {
+    forward();
+  }
+  else if (backCommand) {
+    back();
+  }
+  else if (leftCommand) {
+    left();
+  }
+  else if (rightCommand) {
+    right();
+  }
+  else if (holdCommand) {
+    hold();
+  }
+  else if (ledCommand) {
+    toggleLed();
+  }
+
+  if (obstacle) {
+    hold();
+  }
+
+  if (LT_M || LT_R || LT_L) {
+    hold();
+  }
+}
+
+char readBluetooth() {
+  // bluetooth serial port
+  return Serial.read();
+}
+
+char readIr() {
+  char result;
+
+  if (irrecv.decode(&irResults)) {
+    unsigned long irValue = irResults.value;
+    irrecv.resume();
+
+    switch (irValue) {
+      case IR_UP:
+        return FRW;
+      case IR_DOWN:
+        return BCK;
+      case IR_LEFT:
+        return LFT;
+      case IR_RIGHT:
+        return RGH;
+      case IR_OK:
+        return HLD;
+      case IR_ONE:
+        return LED;
+      default:
+        Serial.println(irValue);
+        break;
+    }
+  }
+
+  return result;
+}
+
+void slowAccelerate() {
+  for (int i = 50; i <= 255; i++) {
+    analogWrite(ENA, i);
+    analogWrite(ENB, i);
+    delay(10);
+  }
+}
+
+void fastAccelerate() {
+  digitalWrite(ENA, HIGH);
+  digitalWrite(ENB, HIGH);
+}
+
+void hold() {
+  digitalWrite(ENA, LOW);
+  digitalWrite(ENB, LOW);
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, LOW);
+}
+
+void forward() {
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, HIGH);
+
+  slowAccelerate();
+}
+
+void back() {
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, HIGH);
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
+
+  slowAccelerate();
+}
+
+void left() {
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, HIGH);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, HIGH);
+
+  fastAccelerate();
+  delay(250);
+  hold();
+}
+
+void right() {
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
+
+  fastAccelerate();
+  delay(250);
+  hold();
+}
+
+void toggleLed() {
+  digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
+}
+
+int measureDistance() {
+  digitalWrite(TRIG, LOW);
+  delayMicroseconds(2);
+  digitalWrite(TRIG, HIGH);
+  delayMicroseconds(20);
+  digitalWrite(TRIG, LOW);
+
+  float distance = pulseIn(ECHO, HIGH);
+  distance = distance / 58;
+
+  return (int) distance;
 }
 
